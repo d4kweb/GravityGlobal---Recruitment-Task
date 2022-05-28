7ffc0902bc045af4ddc4abaa12cf8b7ea31cfa96
d4kweb <tymekpotrzeba@gmail.com>
2022-05-29 01:06:24 +0200

    1.1
    Added alert function.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
-function myalert()
-{
+function openAlert(){
 
-	
+	var Overlay=document.getElementById("overlay");
+	var Alert=document.getElementById("alert");
+	Overlay.style.display="block";
+	Alert.style.display="block";
+	var Clicked=0;
+	if(Clicked==5){
+
+		document.getElementById("alertContent").innerHTML="You have clicked<b>"+Clicked+"Times</b> to related Button if=5";
+
+	}
+	else
+	{
+
+		document.getElementById("alertContent").innerHTML="You have clicked <b>"+Clicked+" Times</b> to related Button else";
+
+	}
+
+}
+function closeAlert(){
+
+	var Overlay=document.getElementById("overlay");
+	var Alert=document.getElementById("alert");
+	Overlay.style.display="none";
+	Alert.style.display="none";
 
 }
