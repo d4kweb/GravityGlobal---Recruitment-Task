787dbc1454b7476d63d1be27d2775043c79dd3e6
d4kweb <tymekpotrzeba@gmail.com>
2022-05-29 03:04:03 +0200

    Counter Function
    Added counter funnction

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,14 @@
+var Clicks=0;
 function openAlert(){
 
 	var Overlay=document.getElementById("overlay");
 	var Alert=document.getElementById("alert");
 	Overlay.style.display="block";
 	Alert.style.display="block";
-	var Clicked=0;
-	if(Clicked==5){
-
-		document.getElementById("alertContent").innerHTML="You have clicked<b>"+Clicked+"Times</b> to related Button if=5";
-
-	}
-	else
-	{
-
-		document.getElementById("alertContent").innerHTML="You have clicked <b>"+Clicked+" Times</b> to related Button else";
-
-	}
-
+	Clicks +=1;
+	if(Clicks==5){document.getElementById("alertContent").innerHTML='You have clicked more than <b>'+Clicks+' Times</b> to related button. <div class="button" onclick="resetCounter();">Reset your counter</div>';
+	Clicks=0;}
+	else{document.getElementById("alertContent").innerHTML="You have clicked <b>"+Clicks+" Times</b> to related button";}
 }
 function closeAlert(){
 
@@ -24,5 +16,11 @@ function closeAlert(){
 	var Alert=document.getElementById("alert");
 	Overlay.style.display="none";
 	Alert.style.display="none";
+
+}
+function resetCounter(){
+
+	Clicks=0;
+	document.getElementById("alertContent").innerHTML="You are clicked 0 Times to related button. You have reset your counter correctly";
 
 }
